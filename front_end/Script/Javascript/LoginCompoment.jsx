73ec8d5e6d09c42200764ef43cb5483c333c8c7b
Van Darkholme <37331560+0Trickster0@users.noreply.github.com>
2022-02-24 18:55:11 -0500

    Update Login Page

--- front_end/Script/Javascript/LoginCompoment.jsx
+++ front_end/Script/Javascript/LoginCompoment.jsx
@@ -18,12 +18,29 @@ class LoginComponent extends React.Component{
     handleChangePassword = (event) => {
         this.setState({password:event.target.value})
     }
+    handleSubmit = (event) => {
+        const{username, password} = this.state
+        if(username == ''){
+            alert('Please input the username')
+            return
+        }
+        if(password == ''){
+            alert('Please input the password')
+            return
+        }
+        alert('Send request:\nusername: ' + username + '\npassword: ' + password)
+    }
+    handleBack = (event) => {
+        window.history.back(-1)
+    }
 
     render(){
         return (
             <div>
                 <LoginInputComponent text='username: ' inputBoxType='text' updateProps = {this.handleChangeUsername}/><br />
-                <LoginInputComponent text='password: ' inputBoxType='password' updateProps = {this.handleChangePassword}/>
+                <LoginInputComponent text='password: ' inputBoxType='password' updateProps = {this.handleChangePassword}/><br/>
+                <button type="submit" onClick={this.handleSubmit} className="loginBtn">Login</button>
+                <button onClick={this.handleBack} className="loginBtn">Back</button>
             </div>
         ) 
     }
