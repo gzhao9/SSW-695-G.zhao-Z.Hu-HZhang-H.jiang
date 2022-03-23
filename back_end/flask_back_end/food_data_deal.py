462f7e348cbb3f5d90c73933514fc749bbb646c1
hygithub3901 <78425325+hygithub3901@users.noreply.github.com>
2022-03-23 14:35:33 +0800

    get food data

--- back_end/flask_back_end/food_data_deal.py
+++ back_end/flask_back_end/food_data_deal.py
@@ -1,6 +1,25 @@
+from flask import Flask,make_response,json
 import json
 import get_food_nutrient
 
-ans = get_food_nutrient.call_API('Cheddar cheese', 'JkdjMHjQobEeEAVSkii5eg1n5NtTKH0AAL0FgXBb')
+app = Flask(__name__)
+
+@app.route('/foodNutrient')
+def foodNutrient():
+    foodName = 'Apple'
+    ans = get_food_nutrient.call_API(foodName, 'JkdjMHjQobEeEAVSkii5eg1n5NtTKH0AAL0FgXBb')
+    nutrientProtein = ans['foods'][1]['foodNutrients'][0]['value']
+    nutrientFat = ans['foods'][1]['foodNutrients'][1]['value']
+    nutrientCarbohydrate = ans['foods'][1]['foodNutrients'][2]['value']
+    data = {
+        'food name:': foodName,
+        'nutrient protein:': nutrientProtein,
+        'nutrient fat': nutrientFat,
+        'nutrient Carbohydrate': nutrientCarbohydrate
+
+    }
+    return make_response(data)
 
 
+if __name__ == '__main__':
+    app.run()
--- back_end/flask_back_end/get_food_nutrient.py
+++ back_end/flask_back_end/get_food_nutrient.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 def call_API(foodName, apiKey):
-    url = f'https://api.nal.usda.gov/fdc/v1/foods/search?api_key={apiKey}&query={foodName}'
+    url = f'https://api.nal.usda.gov/fdc/v1/foods/search?api_key={apiKey}&query={foodName}&requireAllWords=True'
     res = requests.get(url)
     print(res.status_code)
     return res.json()
