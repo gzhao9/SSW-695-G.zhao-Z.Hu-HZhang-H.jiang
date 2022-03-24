46e776783c6c37cee6bebd97ec8035c755502bcf
gzhao9 <74684732+gzhao9@users.noreply.github.com>
2022-03-23 22:29:34 -0400

    Update flask_main.py

--- back_end/flask_back_end/flask_main.py
+++ back_end/flask_back_end/flask_main.py
@@ -1,12 +1,25 @@
-from flask import Flask,make_response,json,render_template
+from contextlib import redirect_stderr
+from flask import Flask,make_response,json,render_template,request,redirect,url_for
 import json
 import get_food_nutrient
+import csv
 #import pandas as pd
 
 app = Flask(__name__)
 
 @app.route('/login')
 def login():
+    username=request.form.get('username')
+    password=request.form.get('password')
+    user_info = {}
+
+    with open('csv_file.csv', mode='r') as inp:
+        reader = csv.reader(inp)
+        user_info = {rows[0]:rows[1] for rows in reader}
+    if user_info[username]==password:
+        return redirect(url_for('helloword'))
+    else:
+        return "<h1>eeeeeeeeeeeee</h1>"
     return render_template('login.html')
 
 @app.route('/FoodPage')
