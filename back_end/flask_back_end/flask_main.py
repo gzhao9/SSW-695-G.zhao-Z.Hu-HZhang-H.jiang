b90ccd6102d064e2a021b2cb85226f88046fb8c2
gzhao9 <74684732+gzhao9@users.noreply.github.com>
2022-03-23 23:00:30 -0400

    Update flask_main.py

--- back_end/flask_back_end/flask_main.py
+++ back_end/flask_back_end/flask_main.py
@@ -1,4 +1,3 @@
-from contextlib import redirect_stderr
 from flask import Flask,make_response,json,render_template,request,redirect,url_for
 import json
 import get_food_nutrient
@@ -7,22 +6,26 @@ import csv
 
 app = Flask(__name__)
 
-@app.route('/login')
+@app.route('/login',methods=['GET','POST'])
 def login():
     username=request.form.get('username')
     password=request.form.get('password')
     user_info = {}
 
-    with open('csv_file.csv', mode='r') as inp:
+    with open('back_end\\flask_back_end\\USER_INFO\\LOGIN.CSV', mode='r') as inp:
         reader = csv.reader(inp)
         user_info = {rows[0]:rows[1] for rows in reader}
-    if user_info[username]==password:
-        return redirect(url_for('helloword'))
-    else:
-        return "<h1>eeeeeeeeeeeee</h1>"
-    return render_template('login.html')
 
-@app.route('/FoodPage')
+    try:
+        result=(user_info[username]!=password)
+        if result:
+            return redirect(url_for('helloword'))
+            
+    except:        
+        result=True
+    return render_template('login.html',result=result)
+
+@app.route('/FoodPage',methods=['GET','POST'])
 def helloword():
     return "<h1>food page</h1>"
 
