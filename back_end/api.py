ef90052862dc71bc4cb82bfe40347ba0d342b4d7
gzhao9 <74684732+gzhao9@users.noreply.github.com>
2022-04-09 00:38:19 -0400

    1

---
+++ back_end/api.py
@@ -0,0 +1,62 @@
+from math import remainder
+from unittest import result
+from flask import Flask,make_response,json,render_template,request,redirect,url_for
+import json
+from rsa import verify
+from functions import *
+
+
+app = Flask(__name__)
+
+
+@app.route('/verifyLogin', methods = ['GET','POST'])
+def login():
+    """
+data={
+password: "aa"
+remember: true
+username: "aa
+}
+    """
+    data = json.loads(request.get_data())   
+    result= {
+        "isSuccess":verify_login(data['username'],data['password']),
+    }
+    return result
+
+
+@app.route('/verifyRegister', methods = ['GET','POST'])
+def login():
+    """
+data=
+{
+	confirm: "aaaaaaaa"
+    email: "aa@a.com"
+    password: "aaaaaaaa"
+}
+    """
+    data = json.loads(request.get_data())   
+    result= {
+        "isSuccess":verify_register(data['confirm'],data['password']),
+    }
+    return result
+
+
+@app.route('/updateUserInfo/<username>', methods = ['GET','POST'])
+def login():
+    """
+data=
+{
+    allergySource: ['fish', 'soy']
+    birthday: "2022-04-07"
+    fullName: "a"
+    height: 1
+    isVegan: false
+    mealPlan: "keepHealth"
+    weight: 1
+}
+    """
+    data = json.loads(request.get_data())   
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
---
+++ back_end/functions.py
@@ -0,0 +1,26 @@
+from math import remainder
+from unittest import result
+from flask import Flask,make_response,json,render_template,request,redirect,url_for
+import json
+import csv
+
+from datetime import datetime
+
+import get_food_nutrient
+
+import flask_db_operate
+
+def verify_login(username,password):
+    userInfo = {
+    'userId': username,
+    'userPassword': password,
+    }
+    return flask_db_operate.findIfInTable('login', 'userId', userInfo['userId'])
+
+def verify_register(username,password):
+    userInfo = {
+        'userId': username,
+        'userPassword': password,
+    }
+    canreg = flask_db_operate.insertLogin(userInfo)
+    return canreg
