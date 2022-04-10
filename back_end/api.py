3e7afa4576677914054cd7413f89b8eafba5b335
gzhao9 <74684732+gzhao9@users.noreply.github.com>
2022-04-10 17:36:10 -0400

    1

--- back_end/api.py
+++ back_end/api.py
@@ -10,13 +10,6 @@ app = Flask(__name__)
 # Login page
 @app.route('/verifyLogin', methods = ['GET','POST'])
 def login():
-    """
-data={
-password: "aa"
-remember: true
-userId: "aa
-}
-    """
     data = json.loads(request.get_data())   
     result= {
         "isSuccess":verify_login(data['userId'],data['password']),
@@ -26,33 +19,75 @@ userId: "aa
 # Register page
 @app.route('/verifyRegister', methods = ['GET','POST'])
 def Register():
-    """
-data=
-{
-	confirm: "aaaaaaaa"
-    email: "aa@a.com"
-    password: "aaaaaaaa"
-}
-    """
     data = json.loads(request.get_data())   
     result= {
         "isSuccess":verify_register(data['confirm'],data['password']),
     }
     return result
 
+# view and Update userInfo page
+@app.route('/updateUserInfo/<userId>', methods = ['GET','POST'])
+def updateUserInfo(userId):
+    data = json.loads(request.get_data())
+    isSuccess=update_user_info(userId,data)
+    result= {
+        "isSuccess":isSuccess,
+    }
+    return result
+
+@app.route('/getUserInfo/<userId>', methods = ['GET','POST'])
+def updateUserInfo(userId):    
+    data = json.loads(request.get_data())
+    
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
 # Home page
 @app.route('/home/<userId>',methods=['GET','POST'])
 def homePage(userId):
     data = json.loads(request.get_data())
     datetime = data['time']
-    """ get user nick name"""
+    #get user nick name
     nickname = flask_db_operate.findEleInTable('userName','userInfo_logs', 'userId', userId)
-    """ get total calcorie """
+    #get total calcorie
     BMR = get_BMR(userId,datetime)
-    """ get meal information in perticular day """
+
     mealres = flask_db_operate.findInTable('mealRecord', 'mealDate', datetime)
 
-    """ get sport information in perticular day """
+
     sportres = flask_db_operate.findInTable('sportRecord', 'sportDate', datetime)
     res_BMR = minus_calorie(mealres, BMR)
 
@@ -63,41 +98,4 @@ def homePage(userId):
         'sportres':sportres,
     }
     return result
-
-
-
-# view and Update userInfo page
-@app.route('/updateUserInfo/<userId>', methods = ['GET','POST'])
-def updateUserInfo(userId):
-    """
-data=
-{
-    allergySource: ['fish', 'soy']
-    birthday: "2022-04-07"
-    fullName: "a"
-    height: 1
-    isVegan: false
-    mealPlan: "keepHealth"
-    weight: 1
-}
-    """
-    data = json.loads(request.get_data())
-
-
-# View meal page
-
-# Insert meal page
-
-# View sport page
-
-# insert sport page
-
-   
-
-
-@app.route('/today_food_record/<userId>', methods = ['GET','POST'])
-def food_record(userId):
-    pass
-
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000)
+"""
--- back_end/functions.py
+++ back_end/functions.py
@@ -5,6 +5,8 @@ import json
 import csv
 
 from datetime import datetime
+from datetime import date
+
 
 import get_food_nutrient
 
@@ -29,7 +31,33 @@ def get_BMR(userId):
     BMR = flask_db_operate.findInTable('userInfo_logs', 'userId', userId)
     return BMR
 
-
-
-def today_food_record(userId):
+def get_food_info(userId,food_name):
     pass
+
+def get_exercise_info(userId,exercise_name):
+    pass
+
+def get_deit_logs(userId,date):
+    pass
+
+def get_exercise_logs(userId,date):
+    pass
+
+def update_user_info(userId,info):
+    age=int(info['date'][:4])-int(info['birthday'][:4])
+    info['userId']=userId
+    info['BMR']=cal_BMR(info['gender'],info['weight'],info['height'],age)
+    
+    #save info to database @hengyuan
+
+def save_exercise_info(userId,info):
+    pass
+
+def save_food_info(userId,info):
+    pass
+
+def cal_BMR(gender,weight,height,age):
+    if gender.lower().startswith('m'):
+        return 66.5 + (13.75 * weight) + (5 * height) - (6.755 * age)
+    else:
+        return 655.1 + (9.6 * weight) + (1.8 * height) - (4.7 * age)
