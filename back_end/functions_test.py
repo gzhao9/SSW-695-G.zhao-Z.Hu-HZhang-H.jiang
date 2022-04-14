f3afbac4838bb73b40eeac29de29f767d4d10ac0
gzhao9 <74684732+gzhao9@users.noreply.github.com>
2022-04-13 20:32:56 -0400

    the test functions

--- back_end/functions_test.py
+++ back_end/functions_test.py
@@ -113,10 +113,10 @@ class test_list_copy(unittest.TestCase):
     #     data = update_food_info(us erId,self.foodInfo)
     #     self.assertTrue(data == 9)
 
-    # def test_update_meal_info(self):
-    #     userId = self.mealdata['userId']
-    #     data = update_meal_info(userId,self.mealdata)
-    #     self.assertTrue(data)
+    def test_update_meal_info(self):
+        userId = self.mealdata['userId']
+        data = update_meal_info(userId,self.mealdata)
+        self.assertTrue(data)
 
 
 """
